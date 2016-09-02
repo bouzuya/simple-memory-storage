d05e7334bde6910bb6dfc013cb64cfc501552222
bouzuya <m@bouzuya.net>
2016-09-02 09:18:31 +0900

    add some tests

--- test/index.ts
+++ test/index.ts
@@ -1,13 +1,65 @@
 import * as assert from 'power-assert';
-import * as proxyquire from 'proxyquire';
-import * as sinon from 'sinon';
 import beater from 'beater';
-import add from '../src/add';
+import { MemoryStorage } from '../src/';
 
 const { test } = beater();
 
-test('add', () => {
-  assert(add(1, 2) === 3);
-  assert(proxyquire);
-  assert(sinon);
+const category = 'MemoryStorage > ';
+
+test(category + 'initial state', () => {
+  const storage = new MemoryStorage();
+  assert(storage.length === 0);
+  assert(storage.key(0) === null);
+  assert(storage.getItem('') === null);
+});
+
+test(category + 'add an item', () => {
+  const storage = new MemoryStorage();
+  storage.setItem('key1', 'value1');
+  assert(storage.length === 1);
+  assert(storage.key(0) === 'key1');
+  assert(storage.getItem('key1') === 'value1');
+});
+
+test(category + 'add 2 items', () => {
+  const storage = new MemoryStorage();
+  storage.setItem('key1', 'value1');
+  storage.setItem('key2', 'value2');
+  assert(storage.length === 2);
+  assert(storage.key(0) === 'key1');
+  assert(storage.getItem('key1') === 'value1');
+  assert(storage.key(1) === 'key2');
+  assert(storage.getItem('key2') === 'value2');
+});
+
+test(category + 'add 2 items and clear', () => {
+  const storage = new MemoryStorage();
+  storage.setItem('key1', 'value1');
+  storage.setItem('key2', 'value2');
+  storage.clear();
+  assert(storage.length === 0);
+  assert(storage.key(0) === null);
+  assert(storage.getItem('key1') === null);
+  assert(storage.key(1) === null);
+  assert(storage.getItem('key2') === null);
+});
+
+test(category + 'add 2 items and remove an item', () => {
+  const storage = new MemoryStorage();
+  storage.setItem('key1', 'value1');
+  storage.setItem('key2', 'value2');
+  storage.removeItem('key1');
+  assert(storage.length === 1);
+  assert(storage.key(0) === 'key2');
+  assert(storage.getItem('key1') === null);
+  assert(storage.getItem('key2') === 'value2');
+});
+
+test(category + 'update an item', () => {
+  const storage = new MemoryStorage();
+  storage.setItem('key1', 'value1');
+  storage.setItem('key1', 'new value');
+  assert(storage.length === 1);
+  assert(storage.key(0) === 'key1');
+  assert(storage.getItem('key1') === 'new value');
 });
